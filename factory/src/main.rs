2dc7f27e1fb5ee69a4b10075f5f3b2fcf072215a
Bader Youssef <ibnbassem@gmail.com>
2024-02-28 10:00:48 -0700

    update completed example

--- factory/src/main.rs
+++ factory/src/main.rs
@@ -14,37 +14,35 @@ trait Process {
 /// What a factory looks like.  Each factory can only process one type of thing, in theory we could
 // "parallelize" these. You can think of these belts as parachains on Polkadot, which execute transactions horizontally.
 // Note that we wanted a multi-process factory, we would need to use dynamic dispatch for both processes (Vec<Box<dyn Process>>)
-struct Factory<P: Process> {
+pub struct Factory<P: Process> {
     factory_id: u32,
     processes: Vec<P>,
     completed_products: HashMap<usize, P::Product>,
 }
 
-
-
 impl<P: Process> Factory<P> {
     // Our "constructor"
-    fn new(factory_id: u32) -> Self {
+    pub fn new(factory_id: u32) -> Self {
         Factory { factory_id, processes: vec![], completed_products: HashMap::new() }
     }
 
-    fn add_new_process(&mut self, process: P) {
-        self.processes.push(process)
-    }
-
-    fn process_all(&mut self) {
+    pub fn process_all(&mut self) {
         // loop thru and run "push_along_belt", then push the products to completed products.
         for (id, process) in self.processes.iter().enumerate() {
             println!("Processing {}...", id);
-            let product = process.push_along_the_belt(process.material());
+            let product: <P as Process>::Product = process.push_along_the_belt(process.material());
             self.completed_products.insert(id, product.clone());
             println!("Process successful, product produced! {:?}...", product);
         }
     }
 
+    pub fn add_new_process(&mut self, process: P) {
+        self.processes.push(process)
+    }
+
     // Run the factory
-    fn run(&mut self) {
-        // run all processes
+    pub fn run(&mut self) {
+        // run all belts
         self.process_all();
     }
 }
@@ -53,17 +51,17 @@ impl<P: Process> Factory<P> {
 // I wonder if there is a better way to do this (there is - macros!)
 
 // Also, the amount here is in tons
-struct IronOre {
-    amount: u32,
+pub struct IronOre {
+    pub amount: u32,
 }
 
 #[derive(Debug, Clone)]
-struct Steel {
-    amount: u32,
+pub struct Steel {
+    pub amount: u32,
 }
 
-struct OreProcess {
-    amount: u32,
+pub struct OreProcess {
+    pub amount: u32,
 }
 
 impl Process for OreProcess {
@@ -71,8 +69,6 @@ impl Process for OreProcess {
     type Product = Steel;
 
     fn push_along_the_belt(&self, material: Self::RawMaterial) -> Self::Product {
-        // This is rather simple,
-        // but we could of course make this much more complex if we wanted to
         Steel { amount: material.amount / 2 }
     }
 
